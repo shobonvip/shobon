b9c66fae642436ae72414aafd29b7535ed7f4f75
Pikachu0310 <sunnon@i.softbank.jp>
2019-01-12 00:42:06 +0900

    Create Discord_BOT.py

---
+++ Discord_BOT/Discord_BOT.py
@@ -0,0 +1,20 @@
+import discord
+
+# 起動時に実行するお
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('起動')
+
+
+# ほ ん へ
+@client.event
+async def on_message(message):
+    if message.author.id == "459591712369541140":
+        client.delete_messages(message)
+    print(message.author.name)
+
+
+# bot起動構文
+client.run('NTMyNTc3MDE3OTg4OTA3MDI4.Dxegpg.ShmleaxXvDJBwF_EpJmnZrhxZBk')
